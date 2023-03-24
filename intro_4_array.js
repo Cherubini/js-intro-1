2dadf6434f3cd7a09ca309f8326526631e4de133
Cherubini <127298157+Cherubini@users.noreply.github.com>
2023-03-24 13:08:49 +0100

    array e ricorsione

---
+++ intro_4_array.js
@@ -0,0 +1,38 @@
+//let pippo = [15, 3, 23, 34, 21];
+
+//  pippo.push(100); //aggiunge in fondo
+
+
+//  const pluto = pippo.pop(); //preleva in fondo
+//  const paperino = pippo . pop();
+
+
+
+//  console.log(pippo);
+//  console.log(pluto);
+//  console.log(paperino)
+
+// pippo.unshift(200); //aggiunge subito in cima
+
+// const paperone= pippo.shift() //preleva all'inizio
+
+//BACO DI JS, non sempre console log funziona
+
+// console.log(pippo);
+// pippo.pop();
+// console.log(pippo);
+
+const arrayOfStrings = ['andrea', 'jing', 'valentina', 'vlad'];
+
+for (let i = 0; i < arrayOfStrings.length; i++) {
+    const element = arrayOfStrings[i];
+    console.log(element);
+}
+
+
+//1) scrivere una funzione che prende come parametro un array di numeri e fa console.log dei numeri con la logica fizzbuzz
+//2) scrivere una funzione che prende come parametro un array di stringhe e logga quelle più piccole di 5 caratteri
+//3) scrivere una funzione che prende come parametro una stringa e restituisce una stringa dei primi 5 caratteri convertiti in maiuscolo
+//4) scrivere una funzione che prende in input come parametro una stringa e restituisce la lunghezza della stringa senza spazi
+//5)scrivere una funzione che prende in input come parametro una stringa e restituisce il numero degli spazi
+ 
